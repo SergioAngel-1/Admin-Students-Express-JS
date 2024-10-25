822ceb6fd900e8a0936a914c8ab73f62c70fa601
Sergio Jauregui <sergioja2003@gmail.com>
2024-10-25 11:48:37 -0500

    Re-Deploy project in Render

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log(`Hello Node.js v${process.versions.node}!`);
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+const expressLayouts = require('express-ejs-layouts');
+const studentRoutes = require('./routes/students');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/main');
+
+// Routes
+app.get('/', (req, res) => res.redirect('/students'));
+app.use('/students', studentRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
